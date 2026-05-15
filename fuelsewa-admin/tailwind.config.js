78dbda5f2f90927ad1fdfbe47d6e32c66d1fa6d3
Sudip_Shrestha <shresthasudip429@gmail.com>
2026-05-15 23:30:42 +0545

    feat: Setup Project

---
+++ fuelsewa-admin/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1D9E75',
+        danger: '#E24B4A',
+        warning: '#BA7517',
+      }
+    },
+  },
+}
